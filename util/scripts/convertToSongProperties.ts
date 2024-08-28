b947358907031a6029e88e7ba2de8cd23cc775a8
sgtjlanc <jamowaind@gmail.com>
2024-08-28 08:29:05 +0100

    push to pc 28/08

--- util/scripts/convertToSongProperties.ts
+++ util/scripts/convertToSongProperties.ts
@@ -1,9 +1,53 @@
 //Import required stuff
 import { MinMaxRecommendation, TargetRecommendation } from "../types/types";
-import { hrIntensityTable } from "../tables";
+import { hrIntensityTable, hrIntensityTimeScaleTable } from "../tables";
 
 //Attempt to best follow figma flowcharts. However bullshittery incoming:
 
-export async function convertToSongProperties(heartRate: number, cadence: number) {
-    
+export function convertToSongProperties(heartRate: number, cadence: number, prevHrZone: string, prevCadence: number, time: number) {
+    //Increment time
+    time++;
+    //Declare needed variables
+    var intensityScore = 0;
+    var zoneChange: string = "";
+    var tempoChange: number;
+    var intensityTempo: number[];
+    for (var zones in hrIntensityTable) {
+        if (!hrIntensityTable.hasOwnProperty(zones)) {
+            continue;
+        }
+        if (
+          heartRate >
+            hrIntensityTable[zones as keyof typeof hrIntensityTable][0] &&
+          heartRate < hrIntensityTable[zones as keyof typeof hrIntensityTable][1]
+        ) {
+            zoneChange = zones
+            break;
+        } 
+        
+    }
+    //Heart Rate zone comparison. If there's a match, check the time spent in that zone.
+    if (zoneChange == prevHrZone) {
+        checkHrDuration(time, prevHrZone);
+    } 
+    //Cadence comparison 
+    if (cadence >= prevCadence*1.1 || cadence <= prevCadence*0.9) {
+        tempoChange = cadence;
+    } else {
+        tempoChange = prevCadence;
+    }
+    return [intensityScore, time]
+}
+function checkHrDuration(time: number, zone: string) {
+    for (var zones in hrIntensityTimeScaleTable) {
+        if (!hrIntensityTable.hasOwnProperty(zones) || zone != zones) {
+          continue;
+        }
+        var element = hrIntensityTimeScaleTable[zones as keyof typeof hrIntensityTimeScaleTable]
+        for (var timestamp in element) {
+            if (time < Number(timestamp)) {
+                return element[timestamp as keyof typeof element];
+            }
+        }
+    }
 }
--- util/tables.ts
+++ util/tables.ts
@@ -1,8 +1,23 @@
 //Format: [Min HR Percent, Min Intensity Score, Max Intensity Score]
 export const hrIntensityTable = {
-    Z1: [0, 64, 20, 30],
-    Z2: [65, 79, 30, 40],
-    Z3: [80, 85, 45, 60],
-    Z4: [86, 92, 65, 75],
-    Z5: [93, 100, 80, 90]
+    'Z1': [0, 64, 20, 30],
+    'Z2': [65, 79, 30, 40],
+    'Z3': [80, 85, 45, 60],
+    'Z4': [86, 92, 65, 75],
+    'Z5': [93, 100, 80, 90]
 };
+//Table to calculate IntensityScore scales based off time spent in different HR Zones. 
+//Format: Zone: {Time in Seconds: Scale}
+//Zone 5 is a special case and is not included in the table.
+export const hrIntensityTimeScaleTable = {
+    'Z3': {
+        900: 1.1,
+        1800: 1.2,
+        3600: 1.3,
+    },
+    'Z4': {
+        900: 1.1,
+        1800: 1.25,
+        2700: 1.45,
+    },
+}
